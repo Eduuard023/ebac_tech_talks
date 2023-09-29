2c5a20fda8906adc5a53323ed6a2e9ca3804ab4d
Eduardo Felipe <eduardofelipe_2000@hotmail.com>
2023-09-29 13:17:10 -0300

    criando o contador

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 AOS.init();
+
+const dataEvento = new Date('Oct 08, 2023 19:00:00');
+const timeStampEvento = dataEvento.getTime();
+
+const contaHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaEvento = timeStampEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 *24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000*60
+
+    const diasAteEvento = Math.floor(distanciaEvento / diaEmMs);
+    const horasAteEvento = Math.floor((distanciaEvento % diaEmMs) / horaEmMs);
+    const minutosAteEvento = Math.floor((distanciaEvento % horaEmMs ) / minutoEmMs);
+    const segundosAteEvento = Math.floor((distanciaEvento % minutoEmMs) / 1000)
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`
+
+    if(diasAteEvento < 0){
+        clearInterval(contaHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+    
+}, 1000)
